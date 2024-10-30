314bfdc65b219ef0790ab9bbd938f19ef30c3154
JamesWainaina <JamesWainaina>
2024-10-30 11:05:23 +0300

    removed unused imports

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React from 'react'
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  }
    return (
      <nav className="bg-blue-900 text-white p-4 fixed w-full z-50">
        <div className="container mx-auto px-4 flex justify-between items-center">
